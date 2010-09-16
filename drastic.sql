a7888ecb77dcdfb508e836c546d365c2d958b65c
Sam Goldstein <sgrock@gmail.com>
2010-09-16 04:08:56 -0700

    Updated SQL

--- drastic.sql
+++ drastic.sql
@@ -166,7 +166,7 @@ INSERT INTO `cached_pages` VALUES (15,'/tags/rails','[18:Article][1:Site][13:Art
 INSERT INTO `cached_pages` VALUES (16,'/tags/ruby','[13:Article][1:Site][9:Article][5:Article][2:Article][20:Article][34:Article][11:Article][22:Article][18:Article][23:Article][21:Article][6:Article][4:Article][3:Article][8:Article][35:Article][10:Article]','2010-09-15 05:46:41',1,NULL);
 INSERT INTO `cached_pages` VALUES (17,'/feed/atom.xml','[1:Section][35:Article][34:Article][23:Article][22:Article][21:Article][20:Article][18:Article][13:Article][11:Article][10:Article][9:Article][8:Article][6:Article][5:Article][3:Article]','2010-09-15 06:59:01',1,NULL);
 INSERT INTO `cached_pages` VALUES (18,'/archives/2009/5','[3:Article][1:Site][4:Article][2:Article][1:Section][35:Article][34:Article][23:Article][22:Article][21:Article][20:Article][18:Article][13:Article][11:Article][10:Article][9:Article][8:Article][6:Article][5:Article]','2010-09-15 05:48:49',1,NULL);
-INSERT INTO `cached_pages` VALUES (19,'/stylesheets/ie.css','','2010-06-28 18:41:23',1,'2010-09-15 05:35:26');
+INSERT INTO `cached_pages` VALUES (19,'/stylesheets/ie.css','','2010-09-15 12:43:37',1,NULL);
 INSERT INTO `cached_pages` VALUES (20,'/2009/5/26/resizing-textareas-as-you-type-with-jquery','[4:Article][1:Site][1:User][20:Article][9:Article][34:Article][11:Article][22:Article][18:Article][23:Article][5:Article][21:Article][13:Article][6:Article][3:Article][2:Article][8:Article][35:Article][10:Article]','2010-09-15 05:47:53',1,NULL);
 INSERT INTO `cached_pages` VALUES (21,'/tags/user%20experience','[1:Site][20:Article][9:Article][34:Article][11:Article][22:Article][18:Article][23:Article][5:Article][21:Article][13:Article][6:Article][4:Article][3:Article][2:Article][8:Article][10:Article]','2010-08-10 17:21:25',1,'2010-09-15 05:35:26');
 INSERT INTO `cached_pages` VALUES (22,'/tags/jquery','[6:Article][1:Site][4:Article][20:Article][9:Article][34:Article][11:Article][22:Article][18:Article][23:Article][5:Article][21:Article][13:Article][3:Article][2:Article][8:Article][35:Article][10:Article]','2010-09-15 05:47:36',1,NULL);
@@ -1143,4 +1143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-09-15 10:58:22
+-- Dump completed on 2010-09-16 11:08:56
